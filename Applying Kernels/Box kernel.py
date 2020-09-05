8fe479d485aacedb485d2361fa8ece927e0647b4
shivamk19 <64491477+shivamk19@users.noreply.github.com>
2020-09-06 00:52:07 +0530

    Added blurred and sharpened

---
+++ Applying Kernels/Box kernel.py
@@ -0,0 +1,39 @@
+from PIL import Image, ImageDraw
+
+# Load image:
+input_image = Image.open("download.jpg")
+input_pixels = input_image.load()
+
+# Box Blur kernel
+box_kernel = [[1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25],
+              [1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25],
+              [1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25],
+              [1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25],
+              [1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25]]
+
+# Select kernel here:
+kernel = box_kernel
+
+# Middle of the kernel
+offset = len(kernel) // 2
+
+# Create output image
+output_image = Image.new("RGB", input_image.size)
+draw = ImageDraw.Draw(output_image)
+
+# Compute convolution between intensity and kernels
+for x in range(offset, input_image.width - offset):
+    for y in range(offset, input_image.height - offset):
+        acc = [0, 0, 0]
+        for a in range(len(kernel)):
+            for b in range(len(kernel)):
+                xn = x + a - offset
+                yn = y + b - offset
+                pixel = input_pixels[xn, yn]
+                acc[0] += pixel[0] * kernel[a][b]
+                acc[1] += pixel[1] * kernel[a][b]
+                acc[2] += pixel[2] * kernel[a][b]
+
+        draw.point((x, y), (int(acc[0]), int(acc[1]), int(acc[2])))
+    
+output_image.save("output_box.png")
