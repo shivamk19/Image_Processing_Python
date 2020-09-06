db950023f6a2f0126b3b6e52e7f9465d0cb3ecf2
shivamk19 <64491477+shivamk19@users.noreply.github.com>
2020-09-06 18:27:41 +0530

    Update Rotation1.py

--- ImageRotation/Rotation1.py
+++ ImageRotation/Rotation1.py
@@ -1,10 +1,7 @@
 from PIL import Image
 import glob
-
-img = Image.new('RGB', (60, 30), color = (10,255,10))
-img.save('r.png')
-#img.show()
-#s = Image.open('rotate.png')
-#a=int(input("Enter the angle by which image has to be rotated"))
-#r = s.rotate(a)
-#r.show()
+img.show()
+s = Image.open('rotate.png')
+a=int(input("Enter the angle by which image has to be rotated"))
+r = s.rotate(a)
+r.show()
